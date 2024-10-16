cb4677caf2e87976fb9b90ffea16765c7d947f91
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2024-10-16 15:23:01 -0300

    chore - (api): algunos cambios

--- api-dea/db/db.sql
+++ api-dea/db/db.sql
@@ -2,39 +2,42 @@ CREATE DATABASE IF NOT EXISTS dea;
 
 USE dea;
 
+CREATE TABLE roles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO roles (name) VALUES ('user');
+INSERT INTO roles (name) VALUES ('admin');
+
 CREATE TABLE users (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL,
-  edited_at TIMESTAMP NOT NULL,
-  rol INT NULL,
-  PRIMARY KEY (id)
+  edited_at TIME NULL,
+  rol INT DEFAULT 1,
   FOREIGN KEY (rol) REFERENCES roles(id)
 );
 
-CREATE TABLE roles (
-  id INT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE dea_points (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
-    ubication VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    lat VARCHAR(255) NOT NULL,
+    lon VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    edited_at TIMESTAMP NOT NULL,
+    edited_at TIME NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 create TABLE posts (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    edited_at TIMESTAMP NOT NULL,
+    edited_at TIME NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
--- api-dea/src/controllers/index.controller.ts
+++ api-dea/src/controllers/index.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { connect } from "../db";
 
-export const indexWelcome = (_req: Request, res: Response): Response => {
+export const indexWelcome = (_req: Request, res: Response) => {
     return res.json({ message: 'Hello World' });
 }
 
@@ -10,3 +10,4 @@ export const ping = async (_req: Request, res: Response): Promise<Response> => {
     const [result] = await pool.query('SELECT 10 + 5')
     return res.json(result)
 }
+
