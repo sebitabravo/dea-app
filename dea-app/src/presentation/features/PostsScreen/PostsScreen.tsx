d82325d231c00a7dbc083a1048997b15199f75f1
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2024-10-16 15:28:08 -0300

    chore - (global): se removieron archvios

---
+++ dea-app/src/presentation/features/PostsScreen/PostsScreen.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export function PostsScreen() {
+    return (
+        <View>
+            <Text>PostsScreen</Text>
+        </View>
+    )
+}
+
+const s = StyleSheet.create({
+    
+})
